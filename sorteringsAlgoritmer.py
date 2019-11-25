7808f31dee637924579017e3d5127f9e5d72aec8
Agger0897 <38247158+Agger0897@users.noreply.github.com>
2019-11-25 14:23:01 +0100

    Selectionsort fix

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,36 +1,38 @@
-from random import shuffle
-
-alist = [3,6,2,5,7,3,3,6]
+list = ("a","B","c","A")
 
 def selectionSort(list):
+
     #Kører alle elementer i listen igennem
-    for i in range(len(list)):
+    for index in range(len(list)):
 
     # Find den mindste værdi i resten af listen
-        min_idx = i
-        for j in range(i+1, len(list)):
+        min_idx = index
+        for j in range(index+1, len(list)):
             if list[min_idx] > list[j]:
                 min_idx = j
 
-    # Bytter rundt på de 2 værdier
-        list[i], list[min_idx] = list[min_idx], list[i]
+    #Bytter rundt på de 2 værdier
+        list[index], list[min_idx] = list[min_idx], list[index]
+
+    #Returnerer listen
     return list
 
 def insertionSort(list):
 
    #Kører alle elementer i listen igennem fra 1 af
-   for index in range(1,len(list)):
+   for index in range(1, len(list)):
 
-     #Vi gemmer hvilken værdi den er nået til
-     #sådan at algoritmen kan fortsætte derfra
-     #når den er færdig med at sammenligne og bytte
-     currentvalue = list[index]
-     position = index
+    #Vi gemmer hvilken værdi den er nået til
+    #sådan at algoritmen kan fortsætte derfra
+    #når den er færdig med at sammenligne og bytte
+    temp = list[index]
+    j = index - 1
 
-     #Sammenligner de 2 værdier
-     #Hvis den til højre er mindre end den til venstre
-     while position>0 and list[position-1]>currentvalue:
+    #Sammenligner de 2 værdier
+    #Hvis den til højre er mindre end den til venstre
+    while (j >= 0 and temp < list[j]):
+
          #Byt rundt på de 2 værdier
-         list[position]=list[position-1]
-         position = position-1
-     list[position]=currentvalue
+         list[j + 1] = list[j]
+         j = j - 1
+    list[j + 1] = temp
